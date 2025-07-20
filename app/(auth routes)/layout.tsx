7215715f34ce1dc830121aeac4f6d5578d6f394a
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-07-20 11:59:53 +0300

    adjusting the app/(auth-routes)/layout.tsx file

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 import css from './AuthLayout.module.css';
+import { useRouter } from 'next/navigation';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
   return <div className={css.authLayout}>{children}</div>;
 }
